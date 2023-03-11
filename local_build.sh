f112abb034994fc7297def2ca95732f3993cc76e
qishipengqsp <shepherdkingqsp@gmail.com>
2023-03-11 23:03:20 +0800

    move mirror to local_build

--- local_build.sh
+++ local_build.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+export MAVEN_OPTS=-Xss10m
+
+# Add huawei mirror for parent repos
+mkdir -p ~/.m2
+echo "<settings>
+  <mirrors>
+    <mirror>
+      <id>huaweicloud</id>
+      <mirrorOf>*</mirrorOf>
+      <url>https://repo.huaweicloud.com/repository/maven/</url>
+    </mirror>
+  </mirrors>
+</settings>" > ~/.m2/settings.xml
+
 mvn clean install -DskipTests
+
