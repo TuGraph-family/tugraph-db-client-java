9978116cb401c673e4fcb3210e9050f3e4fefaf7
qishipengqsp <qishipeng.qsp@antgroup.com>
2023-03-13 12:35:52 +0800

    add github ci

--- local_build.sh
+++ local_build.sh
@@ -2,16 +2,6 @@
 
 # env config
 export MAVEN_OPTS=-Xss10m
-mkdir -p ~/.m2
-echo "<settings>
-  <mirrors>
-    <mirror>
-      <id>alimaven</id>
-      <mirrorOf>central</mirrorOf>
-      <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
-    </mirror>
-  </mirrors>
-</settings>" > ~/.m2/settings.xml
 
 # build all
 mvn clean -f pom.xml install -DskipTests
