fc9920d0bdb6269bae13900680ce629b155d8637
qishipengqsp <shepherdkingqsp@gmail.com>
2023-08-07 15:57:21 +0800

    build with log level warning

--- local_build.sh
+++ local_build.sh
@@ -4,4 +4,4 @@
 export MAVEN_OPTS=-Xss10m
 
 # build all
-mvn --batch-mode --update-snapshots --no-transfer-progress clean -f pom.xml install -DskipTests -Dmaven.javadoc.skip
+mvn --batch-mode --update-snapshots --no-transfer-progress clean -f pom.xml package -DskipTests -Dmaven.javadoc.skip -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN
