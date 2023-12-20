dad9e4f100cf2e421ad0a9de74a29df26f768c53
GongChangYan <1084015508@qq.com>
2023-12-20 11:30:33 +0800

    protoc 3.6.0 -> 3.11.4

--- local_build.sh
+++ local_build.sh
@@ -2,6 +2,16 @@
 
 # env config
 export MAVEN_OPTS=-Xss10m
+mkdir -p ~/.m2
+echo "<settings>
+  <mirrors>
+    <mirror>
+      <id>alimaven</id>
+      <mirrorOf>central</mirrorOf>
+      <url>https://maven.aliyun.com/nexus/content/groups/public/</url>
+    </mirror>
+  </mirrors>
+</settings>" > ~/.m2/settings.xml
 
 # build all
 mvn --batch-mode --update-snapshots --no-transfer-progress clean -f pom.xml package -DskipTests -Dmaven.javadoc.skip -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN
